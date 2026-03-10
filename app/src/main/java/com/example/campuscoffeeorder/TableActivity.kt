a394de4ba04b8394084fc532605c4d79fbf2f77d
Isaac Ferrer <if0999762@otc.edu>
2026-03-10 18:45:48 -0500

    commit 7 image

--- app/src/main/java/com/example/campuscoffeeorder/TableActivity.kt
+++ app/src/main/java/com/example/campuscoffeeorder/TableActivity.kt
@@ -1,6 +1,7 @@
 package com.example.campuscoffeeorder
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
