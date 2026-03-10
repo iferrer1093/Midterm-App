7fc66959de0c5ef84bacde87e12155f71a4fdccf
Isaac Ferrer <if0999762@otc.edu>
2026-03-10 15:35:36 -0500

    commit 3

--- app/src/main/java/com/example/campuscoffeeorder/TableActivity.kt
+++ app/src/main/java/com/example/campuscoffeeorder/TableActivity.kt
@@ -16,5 +16,6 @@ class TableActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+//        cool
     }
 }
