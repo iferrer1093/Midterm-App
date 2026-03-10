c4d2e4609b5491af27df06cc36dd5720b1971682
Isaac Ferrer <if0999762@otc.edu>
2026-03-10 15:03:49 -0500

    commit 2 main layout done

--- app/src/main/java/com/example/campuscoffeeorder/MainActivity.kt
+++ app/src/main/java/com/example/campuscoffeeorder/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.campuscoffeeorder
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+//        val submitButton = findViewById<Button>(R.id.mainSubmitButton)
+//        submitButton.setOnClickListener { v->
+//            Log.d("Click","fart")
+//        }
+
+        val submitButton = object : View.onClickListener {
+            override fun onClick (view: View?) {
+                when (view?.id) {
+                    R.id.mainSubmitButton -> {
+                        val intent = Intent(this@MainActivity, )
+                    }
+                }
+            }
+        }
+
     }
 }
